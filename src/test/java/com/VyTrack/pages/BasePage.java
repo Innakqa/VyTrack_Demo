d3ecd2db613d3df37fe0d1ac09a5730235d2ed72
UmarUzdanov <74148803+UmarUzdanov@users.noreply.github.com>
2021-03-07 21:12:51 -0500

    Added code to BasePage

--- src/test/java/com/VyTrack/pages/BasePage.java
+++ src/test/java/com/VyTrack/pages/BasePage.java
@@ -1,4 +1,31 @@
 package com.VyTrack.pages;
 
-public class BasePage {
+import com.VyTrack.utilities.ConfigurationReader;
+import com.VyTrack.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public abstract class BasePage {
+    public BasePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(id = "prependedInput")
+    public WebElement usernameInputBox;
+
+    @FindBy(id = "prependedInput2")
+    public  WebElement passwordInputBox;
+
+    @FindBy(id = "_submit")
+    public WebElement loginButton;
+
+    @FindBy(xpath = "//*[@id=\"main-menu\"]/ul/li[1]/a/span")
+    public WebElement fleetModule;
+
+    public void login_as_a_driver_config(){
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+        usernameInputBox.sendKeys(ConfigurationReader.getProperty("truckDriver1"));
+        passwordInputBox.sendKeys(ConfigurationReader.getProperty("password"));
+        loginButton.click();
+    }
 }
